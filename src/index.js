05afb93ca680f1ecd9bb1b0c9df328626c65d25c
Rohit <rohit01795@gmail.com>
2020-11-01 22:35:39 +0530

    documentation added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * @exports [useToast]
+ * @exports [Toast]
+ * @exports [toastClass]
+ * @exports [toastFunction]
+ */
 export { useToast } from "hooks";
 export { Toast } from "components";
 export { toastClass, toastFunction } from "renderProps";
