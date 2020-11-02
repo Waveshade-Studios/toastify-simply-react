1aa44bc2a9452b6a9148126dd7d7c05ef91d1298
Rohit <rohit01795@gmail.com>
2020-11-02 21:25:28 +0530

    removed commented code

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,3 @@
-// const { default: useToast } = require("./toast");
-// const { default: useToastFunction } = require("./toastFunction");
-// const { default: useToastType } = require("./toastType");
-
-// exports.useToast = useToast;
-// exports.useToastFunction = useToastFunction;
-// exports.useToastType = useToastType;
-
 import toastHook from "./toast";
 
 const hooks = { useToast: toastHook };
--- src/renderProps/index.js
+++ src/renderProps/index.js
@@ -1,11 +1,3 @@
-// const { default: toastClass } = require("./toast");
-// const { default: toastFunction } = require("./toastFunction");
-// const { default: toastType } = require("./toastType");
-
-// exports.toastClass = toastClass;
-// exports.toastFunction = toastFunction;
-// exports.toastType = toastType;
-
 import toast from "./toast";
 import toastFunc from "./toastFunction";
 
